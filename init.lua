056357c5ff367f619b3f992fb807a5ee376dd76e
Aleksander Spro <abspro@gmail.com>
2025-02-23 11:36:35 +0100

    Add prefix to logger

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+local mq = require("mq")
+local logger = require("knightlinc/Write")
 local map = require("map")
 
+logger.prefix = string.format("\at%s\ax", "[Zoned]")
+logger.postfix = function () return string.format(" %s", os.date("%X")) end
+
 map.AutoMapHeightFilter()
+
+mq.cmd("/easyfind reload")
